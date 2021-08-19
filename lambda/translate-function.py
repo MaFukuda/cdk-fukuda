fb01aac7910ca12045feacecffb3e9b8d53565f3
Masaki Fukuda <fukuda@wni.com>
2021-08-19 15:52:44 +0900

    API Gateway connection available.

--- lambda/translate-function.py
+++ lambda/translate-function.py
@@ -1,4 +1,5 @@
 
+
 import json
 import boto3
 
@@ -7,7 +8,13 @@ translate = boto3.client(service_name='translate')
 def handler(event, context):
     print('request: {}'.format(json.dumps(event)))
     
-    input_text="おはようございます。いい天気ですね。"
+    query_parameters=event.get('queryStringParameters')
+    print('query_parameters: {}'.format(json.dumps(query_parameters)))
+
+    input_text=query_parameters.get("input_text")
+    if input_text is None:
+        input_text="入力がありません。"
+    
     result = translate.translate_text(Text=input_text, 
             SourceLanguageCode="ja", TargetLanguageCode="en")
     
@@ -21,5 +28,4 @@ def handler(event, context):
         },
         'body': 'translated: ' + output_text
     }
-
 
